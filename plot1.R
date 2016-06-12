d69c4d8f2c970cdce5a3db318f7827e71fdb8c9b
ElFuturero <hector.e.meza@gmail.com>
2016-06-12 04:58:51 -0700

    Created plot1.R, started by sourcing Project2Setup.R

---
+++ plot1.R
@@ -0,0 +1,10 @@
+# This is the script to create the first plot for Project 2 of
+# Exploratory Data Analysis in Coursera
+# Student: Hector Meza Delgado
+# Date: June 11, 2016
+
+# First we start by downloading the files, unzipping them, and reading the contents
+# into the variables NEI and SCC. We will do that by sourcing a
+# script that I have prepared called "Project2Setup.R"
+
+source("Project2Setup.R")
