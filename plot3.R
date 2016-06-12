e4bf8b696a97412a743e2dadb2f9b4559a6ef8bc
ElFuturero <hector.e.meza@gmail.com>
2016-06-12 07:21:50 -0700

    Adding dplyr to plot3.R

--- plot3.R
+++ plot3.R
@@ -13,11 +13,30 @@ if(!exists("NEI")){
   source("Project2Setup.R")
 }
 
-# First let's subset the rows relevant to Baltimore, MD (fips == "24510") and for
-# each of the sources and store them in different variables
+# Next we load up our libraries
 
-baltPoint <- subset(NEI, fips == "24510" & type == "POINT")
-baltNP <- subset(NEI, fips == "24510" & type == "NONPOINT")
-baltRoad <- subset(NEI, fips == "24510" & type == "ON-ROAD")
-baltNR <- subset(NEI, fips == "24510" & type == "NON-ROAD")
+library(dplyr)
+library(ggplot2)
 
+# First let's subset the rows relevant to Baltimore, MD (fips == "24510") 
+
+baltEms <- subset(NEI, fips == "24510") %>%
+  tbl_df %>%
+  select(Emissions, year, type) %>%
+  
+  
+
+# baltPoint <- subset(NEI, fips == "24510" & type == "POINT")
+# baltNP <- subset(NEI, fips == "24510" & type == "NONPOINT")
+# baltRoad <- subset(NEI, fips == "24510" & type == "ON-ROAD")
+# baltNR <- subset(NEI, fips == "24510" & type == "NON-ROAD")
+
+# Now we will calculate the total emissions and reassign them to each of the variables.
+# According to the EPA, units are in 10^5 tons.
+
+# baltPoint <- with(baltPoint, tapply(Emissions, year, sum, na.rm = TRUE))
+# baltNP <- with(baltNP, tapply(Emissions, year, sum, na.rm = TRUE))
+# baltRoad <- with(baltRoad, tapply(Emissions, year, sum, na.rm = TRUE))
+# baltNR <- with(baltNR, tapply(Emissions, year, sum, na.rm = TRUE))
+
+# Now we make a 
