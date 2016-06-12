d2c8a83c9356cfe09008dc206393e331a5715623
ElFuturero <hector.e.meza@gmail.com>
2016-06-12 06:33:49 -0700

    Created plot2.R

---
+++ plot2.R
@@ -0,0 +1,14 @@
+# This is the script to create the second plot for Project 2 of
+# Exploratory Data Analysis in Coursera
+# Student: Hector Meza Delgado
+# Date: June 11, 2016
+
+# First we start by downloading the files, unzipping them, and reading the contents
+# into the variables NEI and SCC. We will do that by sourcing a
+# script that I have prepared called "Project2Setup.R"
+# It checks whether the object NEI exists in our workspace, otherwise
+# loads it up
+
+if(!exists("NEI")){
+  source("Project2Setup.R")
+}
