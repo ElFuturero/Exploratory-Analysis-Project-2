3382d4fa743bc01d1b9220beb15c49b31b8e2806
ElFuturero <hector.e.meza@gmail.com>
2016-06-12 04:48:20 -0700

    Created R Proj. Created setup file for project. Ignored "data" directory for Git

---
+++ Project2Setup.R
@@ -0,0 +1,21 @@
+# This file downloads and unzips any necessary files for analysis for
+# the second project of the Exploratory Data Analysis course
+# from Coursera
+# Student: Hector Meza Delgado
+
+# First we want to create a data directory if one hasn't been created
+
+if(!file.exists("data")){
+  dir.create("data")
+}
+
+# Now we want to download and decompress the data if we haven't done so
+
+fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+fileDest <- "./data/project2data.zip"
+
+if (!file.exists(fileDest)){
+  download.file(fileURL, fileDest, method = "curl")
+  # Now let's unzip the file contents
+  unzip(fileDest, exdir = "./data" )
+}
