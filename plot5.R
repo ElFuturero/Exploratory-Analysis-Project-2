4200ae1488961862fa74e3cc1dd0b3508330dc93
ElFuturero <hector.e.meza@gmail.com>
2016-06-12 09:32:39 -0700

    Minor changes in plot5.R and plot6.R to the comments

--- plot5.R
+++ plot5.R
@@ -1,4 +1,4 @@
-# This is the script to create the fourth plot for Project 2 of
+# This is the script to create the fifth plot for Project 2 of
 # Exploratory Data Analysis in Coursera
 # Student: Hector Meza Delgado
 # Date: June 11, 2016
